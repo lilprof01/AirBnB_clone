c1b185855e46804ca2d44670091ec49e31bd45dc
VictorEne-Codes <omalevicene@gmail.com>
2024-11-12 13:14:10 -0800

    fixed errors

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3                                          """defines all common attributes/methods for other classes"""
 from uuid import uuid4
-import datetime
-                                                            
+from datetime import datetime
+
+
 class BaseModel:
     """a class that defines all common attributes/methods for other classes"""
-    def __init__(self, id, created_at, updated_at):                 """a constructor to initialize this class"""
+    def __init__(self):
+        """a constructor to initialize this class"""
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """string representation of class"""                        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """string representation of class"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """updates the updated_at with cirrent time"""
         self.updated_at = datetime.now()
 
-    def __dict__(self):
+    def to_dict(self):
         """creates a dictionary in json format"""
         return {
             "__class__": self.__class__.__name__,
