32f88fb05d5264952b53fbb36c94101188bbb09e
VictorEne-Codes <omalevicene@gmail.com>
2024-11-16 02:37:04 -0800

    task 7

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,27 @@
-#!/usr/bin/python3                                          """defines all common attributes/methods for other classes"""
+#!/usr/bin/python3
+"""defines all common attributes/methods for other classes"""
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
     """a class that defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """a constructor to initialize this class"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """string representation of class"""
@@ -18,6 +30,7 @@ class BaseModel:
     def save(self):
         """updates the updated_at with cirrent time"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """creates a dictionary in json format"""
