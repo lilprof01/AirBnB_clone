a04239de89fd4fd6b64e968349b88a2ed851c0de
lilprof01 <adewumime@gmail.com>
2024-11-16 23:38:51 -0800

    added new classes to file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,22 @@ class FileStorage():
             FileStorage.__objects = {
                 k: current_classes[k.split('.')[0]](**v)
                 for k, v in deserialized.items()}
+
+        def classes(self):
+            """Returns a dictionary of valid classes and their references."""
+            from models.base_model import BaseModel
+            from models.user import User
+            from models.state import State
+            from models.city import City
+            from models.amenity import Amenity
+            from models.place import Place
+            from models.review import Review
+
+            classes = {"BaseModel": BaseModel,
+                       "User": User,
+                       "State": State,
+                       "City": City,
+                       "Amenity": Amenity,
+                       "Place": Place,
+                       "Review": Review}
+            return classes
